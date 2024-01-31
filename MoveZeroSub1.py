57547cf70c9531602bd4fa6de0dfce84da9da7e0
Macrohard Onfire <36289506+pikca-c@users.noreply.github.com>
2024-01-31 21:38:49 +0000

    Added final sol

---
+++ MoveZeroSub1.py
@@ -0,0 +1,9 @@
+def moveZeroes(self, nums):
+        count = 0
+        rtrn = []
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                count = count + 1
+                nums.append(0)
+        for i in range(count):
+            nums.remove(0)
