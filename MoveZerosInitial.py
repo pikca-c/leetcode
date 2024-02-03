aac1b93c58186f83453c77548bf03fde55ccad3a
Macrohard Onfire <36289506+pikca-c@users.noreply.github.com>
2024-02-03 22:16:47 +0000

    Update MoveZerosInitial.py

--- MoveZerosInitial.py
+++ MoveZerosInitial.py
@@ -1,3 +1,14 @@
+#Final submission
+def moveZeroes(self, nums):
+        count = 0
+        rtrn = []
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                count = count + 1
+                nums.append(0)
+        for i in range(count):
+            nums.remove(0)
+        return nums
 #Initial idea
 def moveZeroes(nums):
         count = 0
