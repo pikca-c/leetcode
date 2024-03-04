741b69ce6082c4b2aad0b0bd8025c1b78a387e8b
Macrohard Onfire <36289506+pikca-c@users.noreply.github.com>
2024-03-04 21:42:10 +0000

    Create 709-to_lower_case.py

---
+++ 709-to_lower_case.py
@@ -0,0 +1,8 @@
+#one of the easiest leetcode challenges done
+class Solution(object):
+    def toLowerCase(self, s):
+        return s.lower()
+        """
+        :type s: str
+        :rtype: str
+        """
